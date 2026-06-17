f871cab92f97a30c71ec17f8104019754bbcd125
Mickey-Habesha <mickeygessesse5@gmail.com>
2026-06-17 11:58:43 +0300

    implemented user management module

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,4 +1,50 @@
 package models;
 
-public class Customer {
+public class Customer extends User {
+
+    private String address;
+    private String phoneNumber;
+
+
+    public Customer() {
+    }
+
+
+    public Customer(int id,
+                    String name,
+                    String email,
+                    String password,
+                    String address,
+                    String phoneNumber) {
+
+        super(id, name, email, password);
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("===== Customer Information =====");
+        System.out.println("ID: " + getId());
+        System.out.println("Name: " + getName());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Phone: " + phoneNumber);
+        System.out.println("Address: " + address);
+    }
 }
--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,4 +1,53 @@
 package services;
 
+import models.Admin;
+import models.Customer;
+
+import java.util.ArrayList;
+
 public class UserService {
+
+    private ArrayList<Customer> customers;
+
+    public UserService() {
+        customers = new ArrayList<>();
+    }
+
+    public void registerCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public Customer customerLogin(String email, String password) {
+
+        for (Customer customer : customers) {
+
+            if (customer.getEmail().equals(email)
+                    && customer.getPassword().equals(password)) {
+
+                return customer;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean adminLogin(Admin admin,
+                              String email,
+                              String password) {
+
+        return admin.getEmail().equals(email)
+                && admin.getPassword().equals(password);
+    }
+
+    public void viewCustomers() {
+
+        for (Customer customer : customers) {
+            customer.displayInfo();
+            System.out.println("------------------");
+        }
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
 }
