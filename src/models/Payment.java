91536d1d7758a66b81de14dfeafc89c9e8d15bec
Nardos07 <nardosdemewez487@gmail.com>
2026-06-17 21:32:18 +0300

    Implemented abstract Payment class with polymorphic payment processing

--- src/models/Payment.java
+++ src/models/Payment.java
@@ -1,4 +1,25 @@
 package models;
 
-public class Payment {
+public abstract class Payment {
+
+    protected double amount;
+
+
+    public Payment() {
+    }
+
+
+    public Payment(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public abstract void processPayment();
 }
